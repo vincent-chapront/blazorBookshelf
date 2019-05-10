5921e276d674c3c562fc2608ad153f4cb855eb45
vincent-chapront <vincent.chapront@gmail.com>
2019-05-10 18:17:40 +0200

    Ajout helper

---
+++ BookshelfBlazorPreview/Helpers/StringHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Bookshelf.Helpers
+{
+    public static class StringHelpers
+    {
+        public static string JoinEx(this IEnumerable<string> values, string separator)
+        {
+            return string.Join(separator, values);
+        }
+    }
+}
